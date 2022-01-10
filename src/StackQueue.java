e1595409daa0eddf02686a563298cd6025af89ef
mzalot <69329263+mzalot@users.noreply.github.com>
2022-01-10 09:50:10 -0700

    I finished the dequeue method

--- src/StackQueue.java
+++ src/StackQueue.java
@@ -27,4 +27,30 @@ public class StackQueue <T>{
         counter++;
     }
 
+    //dequeue method
+    public void dequeue(){
+        //if there is one piece of data, pop it
+        if(counter == 1){
+            stack1.pop();
+        }
+        //if stack is greater than one, remove data until last one from stack1 and add to stack2
+        else if(counter > 1){
+            //go through and remove the elements and add to the other stack
+            for(int i = 0; i<counter; i++){
+                stack2.push(stack1.pop());
+            }
+            //remove the piece of data at the bottom
+            stack1.pop();
+            //move all the data back to stack1
+            for(int i = 0; i<counter;i++){
+                stack1.push(stack2.pop());
+            }
+        }
+        //decrease counter
+        //check if counter is above zero
+        if(counter > 0){
+            counter--;
+        }
+    }
+
 }
